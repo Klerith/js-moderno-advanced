31c34ba6b461e503ed09204806b0d4f6e92e2130
Fernando Herrera <fernando.herrera85@gmail.com>
2022-11-23 15:00:25 -0500

    inicio de proyecto

---
+++ main.js
@@ -0,0 +1,21 @@
+import './style.css';
+import javascriptLogo from './javascript.svg';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      
+    
+    </div>
+    
+  </div>
+`;
+
+
